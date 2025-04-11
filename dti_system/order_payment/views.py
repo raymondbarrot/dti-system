8843bebba47c9b01b66211e85196d58c3de78405
Raymond Barrot <raymond.roman@collabera.com>
2025-04-11 13:13:52 +0800

    edit and delete handler

--- dti_system/order_payment/views.py
+++ dti_system/order_payment/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View, CreateView
+from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import OrderPaymentItem
 from .forms import UserRegisterForm, OrderPaymentItemForm
+from django.contrib import messages
 
 # Create your views here.
 class Index(TemplateView):
@@ -13,7 +14,22 @@ class Index(TemplateView):
 class Dashboard(LoginRequiredMixin, View):
     def get(self, request):
         items = OrderPaymentItem.objects.all()
-        return render(request, 'order_payment/dashboard.html', {'items': items})
+
+        new_order_payment = OrderPaymentItem.objects.filter(
+			status="NEW"
+		)
+
+        if new_order_payment.count() > 0:
+            if new_order_payment.count() > 1:
+                messages.info(request, f'There are {new_order_payment.count()} new items.')
+            else:
+                messages.info(request, f'There is {new_order_payment.count()} new item')
+
+        new_order_payment_ids = OrderPaymentItem.objects.filter(
+			status="NEW"
+		).values_list('id', flat=True)
+
+        return render(request, 'order_payment/dashboard.html', {'items': items, 'new_order_payment_ids': new_order_payment_ids})
 
 class SignUpView(View):
     def get(self, request):
@@ -43,3 +59,15 @@ class AddItem(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class EditItem(LoginRequiredMixin, UpdateView):
+    model = OrderPaymentItem
+    form_class = OrderPaymentItemForm
+    template_name = 'order_payment/item_form.html'
+    success_url = reverse_lazy('dashboard')
+
+class DeleteItem(LoginRequiredMixin, DeleteView):
+	model = OrderPaymentItem
+	template_name = 'order_payment/delete_item.html'
+	success_url = reverse_lazy('dashboard')
+	context_object_name = 'item'
